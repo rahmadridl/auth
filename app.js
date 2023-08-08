893097e844a6940ac9b5776ebfb76ff67f35057f
rahmadridl <rahmadridho17@gmail.com>
2023-08-09 01:10:36 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { Sequelize, DataTypes } = require("sequelize");
+
+const app = express();
+app.use(bodyParser.json());
+
+// Konfigurasi Sequelize dan koneksi ke PostgreSQL
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+  }
+);
+
+// Model User
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+// Encrypt password before saving
+User.beforeCreate(async (user) => {
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+});
+
+// API Register
+app.post("/api/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({ name, email, password });
+    res.json({ message: "User registered successfully", user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// API Login
+app.post("/api/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Incorrect password" });
+    }
+
+    const token = jwt.sign({ userId: user.id }, "secretKey", {
+      expiresIn: "1h",
+    });
+    res.json({ message: "Login successful", token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// API Get User Info
+app.get("/api/me", async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, "secretKey");
+    const user = await User.findByPk(decoded.userId, {
+      attributes: ["id", "name", "email"],
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+});
+
+// Start the server
+sequelize.sync().then(() => {
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+});
